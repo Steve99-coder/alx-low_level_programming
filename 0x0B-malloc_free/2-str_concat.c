fa360f4c3e197c259fd7e3c3154a92126d0bdf28
Steve99-coder <stevenderitu99@gmail.com>
2022-04-04 09:00:36 -0700

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,45 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concatenated string
  */
-int main(void)
-{
-    char *s;
 
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int len1 = 0, len2 = 0, i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*(s1 + i))
+		len1++, i++;
+	while (*(s2 + j))
+		len2++, j++;
+	len2++; /* add null terminator to length */
+
+	concat = malloc(sizeof(char) * (len1 + len2)); /*alloc memory*/
+
+	if (concat == NULL) /* validate memory */
+		return (NULL);
+
+	i = 0, j = 0;
+	while (i < len1) /* concatenate */
+	{
+		*(concat + i) = *(s1 + i);
+		i++;
+	}
+	while (j < len2)
+	{
+		*(concat + i) = *(s2 + j);
+		i++, j++;
+	}
+
+	return (concat);
 }
